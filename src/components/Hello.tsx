3a9daddec9b2759ae02c3a02191d444593d97d4d
Shashi Thipparthy <shashi_thipparthy@gallup.com>
2018-01-08 15:06:37 -0600

    added jest and enzyme with sample test

---
+++ src/components/Hello.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export default class Hello extends React.Component {
+
+    constructor(props: any) {
+        super(props);
+    }
+    render() {
+        return <h1>Hello!</h1>;
+    }
+}
